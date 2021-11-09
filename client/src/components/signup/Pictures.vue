7400fadbd44ceca6fde5f414e9fd648304494c11
ravenusmc <mcuddy77@gmail.com>
2021-11-09 10:30:42 -0500

    set up first component for siigngnup page

---
+++ client/src/components/signup/Pictures.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <section>
+      <div class="image"></div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Pictures',
+};
+</script>
+
+<style scoped>
+
+.image {
+  margin: 5% 5% 5% 5%;
+  height: 80vh;
+  width: auto;
+  background-image: url('../../assets/images/Signup_picture.jpg');
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+</style>
--- client/src/views/SignUp.vue
+++ client/src/views/SignUp.vue
@@ -1,16 +1,27 @@
 <template>
   <div>
     <section>
-      <div></div>
+      <Pictures />
     </section>
   </div>
 </template>
 
 <script>
+import Pictures from '@/components/signup/Pictures.vue';
+
 export default {
   name: 'SignUp',
+  components: {
+    Pictures,
+  },
 };
 </script>
 
 <style scoped>
+section {
+  border: 2px solid red;
+  height: 90vh;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+}
 </style>
