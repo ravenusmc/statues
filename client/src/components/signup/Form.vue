2df11cb6717d19e2c4c11ffa74f361005ff0bc6e
ravenusmc <mcuddy77@gmail.com>
2021-11-09 10:41:57 -0500

    set up basic form

---
+++ client/src/components/signup/Form.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <form>
+      <div class="field">
+        <input
+          class="input is-primary is-rounded"
+          type="name"
+          placeholder="Name"
+        />
+      </div>
+      <div class="field">
+        <input
+          class="input is-primary is-rounded"
+          type="email"
+          placeholder="email"
+        />
+      </div>
+      <div class="field">
+        <input
+          class="input is-primary is-rounded"
+          type="password"
+          placeholder="Password"
+        />
+      </div>
+      <div class="field">
+        <input
+          class="input is-primary is-rounded"
+          type="password"
+          placeholder="Confirm Password"
+        />
+      </div>
+      <button class="button is-success">Success</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Form',
+};
+</script>
+
+<style scoped>
+</style>
--- client/src/views/SignUp.vue
+++ client/src/views/SignUp.vue
@@ -2,25 +2,27 @@
   <div>
     <section>
       <Pictures />
+      <Form />
     </section>
   </div>
 </template>
 
 <script>
 import Pictures from '@/components/signup/Pictures.vue';
+import Form from '@/components/signup/Form.vue';
 
 export default {
   name: 'SignUp',
   components: {
     Pictures,
+    Form,
   },
 };
 </script>
 
 <style scoped>
 section {
-  border: 2px solid red;
-  height: 90vh;
+  height: 95vh;
   display: grid;
   grid-template-columns: 1fr 1fr;
 }
