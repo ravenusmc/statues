1356477e3736029635416f3279225352d84cb555
ravenusmc <mcuddy77@gmail.com>
2021-11-12 09:33:33 -0500

    setting up form

--- client/src/components/signup/Form.vue
+++ client/src/components/signup/Form.vue
@@ -1,26 +1,45 @@
 <template>
   <div>
     <section>
-      <h1 class='center signup-title title-size font'>Sign Up</h1>
-      <form>
+      <h1 class="center signup-title title-size font">Sign Up</h1>
+      <form @submit="signup">
         <div class="field">
           <input
             class="input is-primary is-rounded"
             type="name"
-            placeholder="Name"
+            v-model="firstName"
+            placeholder="First Name"
+          />
+        </div>
+        <div class="field">
+          <input
+            class="input is-primary is-rounded"
+            type="name"
+            v-model="lastName"
+            placeholder="Last Name"
           />
         </div>
         <div class="field">
           <input
             class="input is-primary is-rounded"
             type="email"
+            v-model="email"
             placeholder="email"
           />
         </div>
         <div class="field">
           <input
             class="input is-primary is-rounded"
+            type="name"
+            v-model="userName"
+            placeholder="UserName"
+          />
+        </div>
+        <div class="field">
+          <input
+            class="input is-primary is-rounded"
             type="password"
+            v-model="password"
             placeholder="Password"
           />
         </div>
@@ -28,6 +47,7 @@
           <input
             class="input is-primary is-rounded"
             type="password"
+            v-model="confirmPassword"
             placeholder="Confirm Password"
           />
         </div>
@@ -40,6 +60,48 @@
 <script>
 export default {
   name: 'Form',
+  data() {
+    return {
+      firstName: '',
+      lastName: '',
+      email: '',
+      userName: '',
+      password: '',
+      confirmPassword: '',
+    };
+  },
+  methods: {
+    signup(evt) {
+      evt.preventDefault();
+      if (this.firstName === '') {
+        alert('First name must be entered');
+      } else if (this.lastName === '') {
+        alert('Last name must be entered');
+      } else if (this.email === '') {
+        alert('Email must be entered');
+      } else if (this.userName === '') {
+        alert('User name must be entered');
+      } else if (this.password === '') {
+        alert('Password must be entered');
+      } else if (this.password !== this.confirmPassword) {
+        alert('Passwords must be the same');
+      } else if (this.password.length < 6) {
+        alert('Password must be at least 6 characters long');
+      } else {
+        const payload = {
+          firstName: this.firstName,
+          lastName: this.lastName,
+          email: this.email,
+          userName: this.userName,
+          password: this.password,
+          confirmPassword: this.confirmPassword,
+        };
+        console.log(payload);
+      }
+
+      // this.$store.dispatch('common/login', { payload });
+    },
+  },
 };
 </script>
 
@@ -56,5 +118,4 @@ section {
   text-transform: uppercase;
   font-weight: bold;
 }
-
 </style>
