a2cb366577174d3cc941b1acd573171e40bf5906
ravenusmc <mcuddy77@gmail.com>
2021-11-18 15:57:55 -0500

    user is now able to sign up to site

--- server/app.py
+++ server/app.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 
 # Files that I built
 from db import *
+from user import *
 
 # configuration
 DEBUG = True
@@ -15,18 +16,18 @@ app.config.from_object(__name__)
 CORS(app, resources={r'/*': {'origins': '*'}})
 
 # This route will allow new users to register on the site
+
+
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
         db = Connection()
         post_data = request.get_json()
-        print(post_data)
-        # email = post_data['email']
-        # username = post_data['username']
-        # password = post_data['password']
-        # user = User(email, username, password)
-        # hashed = db.encrypt_pass(post_data)
-        # user_created = db.insert(user, hashed)
+        user = User(post_data['firstName'], post_data['lastName'], post_data['email'], 
+        post_data['userName'], post_data['password'])
+        hashed = db.encrypt_pass(post_data)
+        user_created = db.insert(user, hashed)
+        print(user_created)
         # return jsonify(user_created)
         return jsonify('7')
 
--- server/db.py
+++ server/db.py
@@ -15,3 +15,19 @@ class Connection():
                                             port=3306,
                                             database='flags')
         self.cursor = self.conn.cursor()
+
+    def encrypt_pass(self, post_data):
+        password = post_data['password'].encode('utf-8')
+        hashed = bcrypt.hashpw(password, bcrypt.gensalt())
+        return hashed
+
+    # This method will insert a new user into the database.
+    def insert(self, user, hashed):
+        user_created = True
+        self._SQL = """insert into users
+          (firstName, lastName, email, username, password)
+          values
+          (%s, %s, %s, %s, %s)"""
+        self.cursor.execute(self._SQL, (user.first_name, user.last_name, user.email, user.username, hashed))
+        self.conn.commit()
+        return user_created
