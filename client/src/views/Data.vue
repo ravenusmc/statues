20c9fdc3be80d72b1fcdc9b646a9a4f983127ae0
ravenusmc <mcuddy77@gmail.com>
2021-12-08 10:53:40 -0500

    renamed some files

--- client/src/views/Data.vue
+++ client/src/views/Data.vue
@@ -1,16 +1,20 @@
 <template>
   <div>
     <Intro />
+    <hr>
+    <GraphOne />
   </div>
 </template>
 
 <script>
 import Intro from '@/components/data/Intro.vue';
+import GraphOne from '@/components/data/GraphOne.vue';
 
 export default {
   name: 'Data',
   components: {
     Intro,
+    GraphOne, 
   },
 };
 </script>
--- server/app.py
+++ server/app.py
@@ -47,6 +47,23 @@ def login():
         login_values['user'] = user
     return jsonify(login_values)
 
+@app.route('/graphOne', methods=['GET', 'POST'])
+def graphOne():
+    if request.method == 'POST':
+        db = Connection()
+        post_data = request.get_json()
+        username = post_data['userName']
+        password = post_data['password']
+        login_values = {}
+        # Checking to see if the user is in the database
+        login_flag, not_found, password_no_match, user = db.check(
+            username, password)
+        login_values['login_flag'] = login_flag
+        login_values['Not_found'] = not_found
+        login_values['Password_no_match'] = password_no_match
+        login_values['user'] = user
+    return jsonify(login_values)
+
 
 if __name__ == '__main__':
     app.run()
