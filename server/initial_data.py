5e7e41c6253a7123d079765279cbbd751fd7bca0
ravenusmc <mcuddy77@gmail.com>
2021-12-17 14:12:34 -0500

    working on building first graph

--- server/initial_data.py
+++ server/initial_data.py
@@ -1,4 +1,4 @@
-# This file will deal with a lot of the initial data sets for this project. The file 
+# This file will deal with a lot of the initial data sets for this project. The file
 # will essentially build the initial dataset.
 
 # importing supporting libraries
@@ -10,13 +10,18 @@ import datetime
 from clean import *
 
 # Class builds the initial data set - which I will then place into the store on the client side.
+
+
 class InitialData():
 
     def build_north_south_graph(self):
         clean = Clean()
         statues_data_subset = clean.clean_data()
-        print(statues_data_subset.head())
+        sides = ['North', 'South']
+        for side in sides: 
+            number_of_statues = len(statues_data_subset[(statues_data_subset.Side == side)])
+            print(number_of_statues)
 
-test = Data() 
+
+test = InitialData()
 test.build_north_south_graph()
-
