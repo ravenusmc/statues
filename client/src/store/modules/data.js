c4816d3b7a20734f8c5c8a59c53926de979e0a7e
ravenusmc <mcuddy77@gmail.com>
2021-12-18 11:21:14 -0500

    set up initial store for graph data

---
+++ client/src/store/modules/data.js
@@ -0,0 +1,51 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import axios from 'axios';
+
+Vue.use(Vuex)
+
+const state = {
+	firstGraphDataSetInitial: [['Side', 'Count'], ['North', 43], ['South', 1722]],
+};
+
+const getters = {
+	firstGraphDataSetInitial: state => state.firstGraphDataSetInitial,
+};
+
+const actions = {
+
+	// fetchFileInformation: ({ commit }, { formData }) => {
+	// 	const path = 'http://localhost:5000/fetch_File_Information';
+	// 	axios.post(path, formData, {
+	// 		headers: {
+	// 			'Content-Type': 'multipart/form-data'
+	// 		}
+	// 	})
+	// 		.then((res) => {
+	// 			commit('setFileName', res.data.file_name)
+	// 			commit('setColumns', res.data.column_names)
+	// 			commit('setShowDataArea', true)
+	// 		})
+	// 		.catch(error => {
+	// 			console.log(error);
+	// 		})
+	// },
+
+};
+
+
+const mutations = {
+
+	setColumns(state, data) {
+		state.columns = data
+	},
+
+};
+
+export default {
+	namespaced: true,
+	state,
+	getters,
+	actions,
+	mutations
+};
