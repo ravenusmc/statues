edc1d6baad6ebd54adb3ad3cf19066e65e67b842
ravenusmc <mcuddy77@gmail.com>
2021-12-19 11:56:34 -0500

    added first graph

--- client/src/components/data/GraphOne.vue
+++ client/src/components/data/GraphOne.vue
@@ -1,17 +1,43 @@
-<template>	
-	<div>
-		<h1>Data one</h1>
-	</div>
+<template>
+  <div>
+    <GraphCard
+      :typeOne="typeOne"
+      :data="firstGraphDataSetInitial"
+      :options="chartOptionsOne"
+    />
+  </div>
 </template>
 
 <script>
-import GraphOneDiscussion from '@/components/data/GraphOneDiscussion.vue';
+import { mapGetters } from 'vuex';
+import GraphCard from '@/components/Graphs/GraphCard.vue';
 
 export default {
-	name: 'GraphOne',
-}
+  name: 'GraphOne',
+  components: {
+    GraphCard,
+  },
+  computed: {
+    ...mapGetters('data', ['firstGraphDataSetInitial']),
+  },
+  data() {
+    return {
+      typeOne: 'ColumnChart',
+      chartOptionsOne: {
+        title: 'Statues in North Vs South',
+        legend: { position: 'top' },
+        colors: ['#05ce78'],
+        height: 500,
+        vAxis: {
+          viewWindow: {
+            min: 0,
+          },
+        },
+      },
+    };
+  },
+};
 </script>
 
 <style scoped>
-
 </style>
