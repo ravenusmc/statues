d70d543c41836ebb6f4bc1c5d85f0e9135b80be4
ravenusmc <mcuddy77@gmail.com>
2021-12-23 16:42:35 -0500

    minor changes

--- client/src/components/data/FormOne.vue
+++ client/src/components/data/FormOne.vue
@@ -32,14 +32,7 @@ export default {
       // this.startDate = moment(this.startDate).format('M/D/YYYY h:mm:ss A');
       // this.endDate = moment(this.endDate).format('M/D/YYYY h:mm:ss A');
       // const payload = {
-      //   category: this.category,
-      //   state: this.state,
-      //   currency: 'USD',
-      //   country: 'US',
-      //   startDate: this.startDate,
-      //   endDate: this.endDate,
-      //   moneyGoal: this.moneyGoal,
-      //   backers: this.backers,
+      //   year: this.year,
       // };
       // this.fireActions({ payload });
     },
