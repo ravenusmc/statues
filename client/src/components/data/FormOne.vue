a2f8cd7e665f711d8c30092031731d9e6f520bad
ravenusmc <mcuddy77@gmail.com>
2021-12-24 11:27:23 -0500

    working on form for first graph

--- client/src/components/data/FormOne.vue
+++ client/src/components/data/FormOne.vue
@@ -2,12 +2,21 @@
   <div>
     <form @submit="submitForm">
       <div>
-        <label for="year">Select Year (10-100):</label>
+        <label for="yearOne">Enter First Year (1870-):</label>
         <input
           type="number"
           id="year"
-					v-model="year"
-          name="year"
+          v-model="yearOne"
+          name="yearOne"
+          min="1870"
+          max="1950"
+        />
+        <label for="yearTwo">Enter Second Year (1870-100):</label>
+        <input
+          type="number"
+          id="year"
+          v-model="yearTwo"
+          name="yearTwo"
           min="1870"
           max="1950"
         />
@@ -22,13 +31,15 @@ export default {
   name: 'FormOne',
   data() {
     return {
-      year: 1870,
+			yearOne: 1870,
+			yearTwo: 1870,
     };
   },
   methods: {
     submitForm(evt) {
-			evt.preventDefault();
-			console.log(this.year);
+      evt.preventDefault();
+			console.log(this.yearOne);
+			console.log(this.yearTwo)
       // this.startDate = moment(this.startDate).format('M/D/YYYY h:mm:ss A');
       // this.endDate = moment(this.endDate).format('M/D/YYYY h:mm:ss A');
       // const payload = {
