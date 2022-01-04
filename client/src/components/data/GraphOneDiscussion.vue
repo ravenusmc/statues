bff163bd417e014d1a944bf9f5be4b68322bee48
ravenusmc <mcuddy77@gmail.com>
2022-01-04 18:41:51 -0500

    finished discussion paragraph

--- client/src/components/data/GraphOneDiscussion.vue
+++ client/src/components/data/GraphOneDiscussion.vue
@@ -1,14 +1,19 @@
 <template>
-  <div class='graph-discussion'>
+  <div class="graph-discussion">
     <h1 class="center font">Discussion</h1>
     <p>
       <span class="responsive-phrase-large">The graph to the right...</span>
       <span class="responsive-phrase-small">The below graph...</span>
-      There's one thing that stands out to me on the graph. The South always appears 
-      to have more statues than the North. The North won the war. I almost would have thought 
-      that the North would have had more statues up until about 1890. That is very clearly 
-      not the case. From the start, according to the data, the South had way more statues than 
-      the North did.
+      There's one thing that stands out to me on the graph. The South always
+      appears to have more statues than the North. The North won the war. I
+      almost would have thought that the North would have had more statues up
+      until about 1890. That is very clearly not the case. From the start,
+      according to the data, the South had way more statues than the North did.
+      The South, from the start, appears to have been much more willing to pay
+      tribute to the fallen than the North. Still this does not make much sense
+      to me. The Lost Cause Myth I believe was developed towards the end of the
+      19th century. I'd almost expect to have more statues in the North up until
+      that point.
     </p>
   </div>
 </template>
@@ -20,6 +25,10 @@ export default {
 </script>
 
 <style scoped>
+.responsive-phrase-large {
+  display: block;
+}
+
 .responsive-phrase-small {
   display: none;
 }
@@ -29,7 +38,7 @@ export default {
 }
 
 /* Responsive media quries */
-@media only all and (max-width: 768px) {
+@media only all and (max-width: 999px) {
   .responsive-phrase-large {
     display: none;
   }
