6bcc9e3836abe8c83028efffabf12e1bdc92b237
ravenusmc <mcuddy77@gmail.com>
2022-02-26 11:08:28 -0500

    started working on fifth graph

---
+++ client/src/components/data/graphfive/GraphFive.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <GraphCard
+      :typeOne="typeOne"
+      :data="YearsStatuesRemovedData"
+      :options="chartOptionsOne"
+    />
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+import GraphCard from '@/components/Graphs/GraphCard.vue';
+
+export default {
+  name: 'GraphOne',
+  components: {
+    GraphCard,
+  },
+  computed: {
+    ...mapGetters('data', ['removedStatuesData']),
+  },
+  data() {
+    return {
+      typeOne: 'ColumnChart',
+      chartOptionsOne: {
+        title: 'Top 5 States that Removed Statues',
+        legend: { position: 'top' },
+        colors: ['#f24867'],
+        height: 500,
+        animation: {
+          duration: 1000,
+          easing: 'linear',
+        },
+        vAxis: {
+          viewWindow: {
+            min: 0,
+          },
+        },
+      },
+    };
+  },
+};
+</script>
