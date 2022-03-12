943f267a5989f0982da9fc65dfb8ec813c3ac6f5
ravenusmc <mcuddy77@gmail.com>
2022-03-12 11:58:47 -0500

    started work on building sixth graph

---
+++ client/src/components/data/graphsix/GraphSix.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <GraphCard
+      :typeOne="typeOne"
+      :data="firstGraphDataSetInitial"
+      :options="chartOptionsOne"
+    />
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+import GraphCard from '@/components/Graphs/GraphCard.vue';
+
+export default {
+  name: 'GraphSix',
+  components: {
+    GraphCard,
+  },
+  computed: {
+    ...mapGetters('data', ['firstGraphDataSetInitial']),
+  },
+  data() {
+    return {
+      typeOne: 'ColumnChart',
+      chartOptionsOne: {
+        title: 'Statue Type Over Time',
+        legend: { position: 'top' },
+        colors: ['#f24867'],
+        height: 500,
+        animation: {
+          duration: 1000,
+          easing: 'linear',
+        },
+        vAxis: {
+          viewWindow: {
+            min: 0,
+          },
+        },
+      },
+    };
+  },
+};
+</script>
---
+++ client/src/components/data/graphsix/GraphSixDiscussion.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="graph-discussion">
+    <h1 class="center font heading-title">Graph Six Discussion</h1>
+    <p>
+      <span class="responsive-phrase-large">The graph to the right...</span>
+      <span class="responsive-phrase-small">The below graph...</span>
+    </p>
+    <br />
+    <p>
+    </p>
+    <br />
+    <p>
+
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GraphSixDiscussion',
+};
+</script>
+
+<style scoped>
+.responsive-phrase-large {
+  display: block;
+}
+
+.responsive-phrase-small {
+  display: none;
+}
+
+.graph-discussion {
+  margin: 0 10% 0 10%;
+}
+
+.heading-title {
+  font-weight: bolder;
+  font-size: 2em;
+  text-transform: uppercase;
+  margin-bottom: 15px;
+}
+
+/* Responsive media quries */
+@media only all and (max-width: 999px) {
+  .responsive-phrase-large {
+    display: none;
+  }
+
+  .responsive-phrase-small {
+    display: block;
+  }
+}
+</style>
