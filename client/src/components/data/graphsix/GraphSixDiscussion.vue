b534c487f00f4aeb8dd1323cdbb5a18dd5198ea3
ravenusmc <mcuddy77@gmail.com>
2022-03-25 14:28:17 -0400

    added discussin section to sixth graph

--- client/src/components/data/graphsix/GraphSixDiscussion.vue
+++ client/src/components/data/graphsix/GraphSixDiscussion.vue
@@ -7,11 +7,16 @@
     </p>
     <br />
     <p>
+      I don't believe there's anything super interesting on this graph - like how 
+      a lot of the other drive home the point that there's a lot more statues in the South then 
+      there are in the North. What this graph does show is that it appears that the most common way
+      to honor the dead is through monuments. This makes sense. I think that throughout history 
+      this would be the most common way to honor those who fought. People want to be seen in granite 
+      or marble - to truly last through the ages. After that comes the roads and schools being named - even 
+      the name of a county. That monumnet though is going to last a long time and really show people how 
+      important something is when you make a large statue of it. 
     </p>
     <br />
-    <p>
-
-    </p>
   </div>
 </template>
 
