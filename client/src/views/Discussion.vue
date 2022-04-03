778a8c41ea37a22956d2f3b76dc33c8944d8b655
ravenusmc <mcuddy77@gmail.com>
2022-04-03 14:59:34 -0400

    working on discussion page

--- client/src/views/Discussion.vue
+++ client/src/views/Discussion.vue
@@ -1,22 +1,22 @@
 <template>
   <div>
     <section>
-      <div>
+      <div class="graph_discussion_div">
         <h6>Discuss Graph One</h6>
       </div>
-      <div>
+      <div class="graph_discussion_div">
         <h6>Discuss Graph Two</h6>
       </div>
-      <div>
+      <div class="graph_discussion_div">
         <h6>Discuss Graph Three</h6>
       </div>
-      <div>
+      <div class="graph_discussion_div">
         <h6>Discuss Graph Four</h6>
       </div>
-      <div>
+      <div class="graph_discussion_div">
         <h6>Discuss Graph Five</h6>
       </div>
-      <div>
+      <div class="graph_discussion_div">
         <h6>Discuss Graph Six</h6>
       </div>
     </section>
@@ -31,6 +31,21 @@ export default {
 
 <style lang="css" scoped>
 section {
-	grid-template-columns: 1fr 1fr;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  border: 2px solid red;
+  margin-top: 100px;
+  margin-bottom: 100px;
+  align-items: center;
+  justify-items: center;
+}
+
+.graph_discussion_div {
+  border: 2px solid black;
+  text-align: center;
+  width: 50%;
+  margin-top: 50px;
+  margin-bottom: 50px;
+  padding: 25px;
 }
 </style>
